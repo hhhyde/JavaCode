ebb75d474f7bd0e1fa242bf816d00d882cc9d6ee
hhhyde <kj85500@gmail.com>
2014-02-15 21:01:32 +0800

    init
    Signed-off-by: hhhyde <kj85500@gmail.com>

---
+++ src/a8_7/a8_7C.java
@@ -0,0 +1,8 @@
+package a8_7;
+
+import java.awt.List;
+import java.util.Map;
+
+public class a8_7C {
+	
+}
---
+++ src/a9_24/FileTestTest.java
@@ -0,0 +1,31 @@
+package a9_24;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FileTestTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+//	@Test
+//	public void testRead() {
+//		try {
+//			System.out.println(FileTest.read("C:/Users/ke/Desktop/test.txt"));
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+
+}
